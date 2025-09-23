d49a963e61085ef924376b8d82b9d33d102574fa
Ani <turboji007@gmail.com>
2025-09-23 19:49:56 +0530

    added style variables and code structure.

---
+++ app/layout.tsx
@@ -0,0 +1,68 @@
+import type { Metadata } from "next";
+import { Urbanist, Syne } from "next/font/google";
+import "./globals.css";
+import NavBar from '@/components/NavBar';
+
+import  localFont  from 'next/font/local';
+
+const centuryGothic = localFont({
+  variable: '--century-gothic',
+  display: 'swap',
+  src: [
+    {
+      path: '/font/century_gothic/CenturyGothicBold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '/font/century_gothic/CenturyGothic.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ]
+});
+
+
+// Configure Google Fonts
+const urbanist = Urbanist({
+  subsets: ["latin"],
+  variable: "--urbanist",
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+// const gothicA1 = Gothic_A1({
+//   subsets: ["latin"],
+//   variable: "--gothic",
+//   display: "swap",
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Gothic A1 requires weight specification
+// });
+
+const syne = Syne({
+  subsets: ["latin"],
+  variable: "--syne",
+  weight: ["400", "500"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Kaushalay Home Learning",
+  description: "Nurture Skills, Build Future",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${urbanist.variable} ${centuryGothic.variable} ${syne.variable} antialiased`}
+      >
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,23 @@
+const Hero = () => {
+  return (
+    <div className="pt-14  section bg-green-400 w-secreen h-screen ">
+
+      <main className="w-full h-full bg-cyan-500 flex flex-col items-center justify-start   ">
+
+        <header className="font-gothic font-bold text-5xl mt-8">
+          <h1>
+            <span>Nurturing </span>
+            <span>Skills, </span>
+            <span>Build Future</span>
+          </h1>
+        </header>
+
+
+
+
+      </main>
+    </div>
+  );
+};
+
+export default Hero;
