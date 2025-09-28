4fc3c3ae7cdcc11e0f332fe7351fac6ad562db52
Ani <turboji007@gmail.com>
2025-09-28 18:56:12 +0530

    completed hero section

--- components/WhyChooseUs.tsx
+++ components/WhyChooseUs.tsx
@@ -1,6 +1,25 @@
+import Image from "next/image";
+
 const WhyChooseUs = () => {
   return (
-    <div className="w-screen h-screen section">
+    <div className="bg-red-500 w-screen min-h-screen section">
+      <header>
+        <div className="flex flex-col">
+          
+          <Image
+            src="/upperHyphen.png"
+            alt="Why Choose Us"
+            width={50}
+            height={50}
+            className="w-full h-auto"
+          />
+
+          <h1 className="text-4xl font-bold text-white text-center mb-8">
+            Why Choose Us
+          </h1>
+        </div>
+      </header>
+
       <div className="container">
         <div className="box red"></div>
         <div className="box purple"></div>
