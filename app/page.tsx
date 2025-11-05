d55163ca22914f87d11a050c1b738d2177651ead
Ani <turboji007@gmail.com>
2025-11-05 20:08:43 +0530

    integrated main website

--- app/page.tsx
+++ app/page.tsx
@@ -27,15 +27,4 @@ export default function Home() {
 
     </div>
   );
- }
-
-
-// import React from 'react'
-
-// const page = () => {
-//   return (
-//     <div>page</div>
-//   )
-// }
-
-// export default page
+}
